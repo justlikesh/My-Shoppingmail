4a76f6a0ce773697684ab8c620fae5ac907a2ac8
justlikesh <lpsac@naver.com>
2024-09-20 15:11:20 +0900

    Feat : ajax를 사용한 삭제기능 구현
    - method 속성에 어떤 method 로 요청할지 설정
    - headers란에 전송할 데이터 타입 설정(application.json) 으로 적어야 object 형태로 전달됌
    - body 속성에 어떤 서버로 보낼지 설정
    - 전송할 데이터가 많으면 array, object 안에 담아서 전송(JSON.stringify()안에 넣어서 전달)
    - ajax로 application/json 타입의 데이터를 보냈다면 API 구현시 파라미터를 @RequestBody 로 구현 타입은 자바스크립트 object -> Map

--- src/main/java/com/apple8/_shop/item/ItemController.java
+++ src/main/java/com/apple8/_shop/item/ItemController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -74,6 +75,12 @@ public class ItemController {
         item.setPrice(price);
         itemRepository.save(item);
 
+        return "redirect:/list";
+    }
+
+    @PostMapping("/test1")
+    String test1(@RequestBody Map<String, Object> body){
+        System.out.println(body);
         return "redirect:/list";
     }
 //    itemRepository.save()
